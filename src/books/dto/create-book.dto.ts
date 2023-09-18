312e9ad9ecb4e3735dbf95a4d246d258ef156ea3
aruay99 <aruaiberdk@gmail.com>
2023-09-18 23:07:24 +0300

    docs(openapi): add createbookdto examples

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,17 +1,22 @@
 import { IsDate, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBookDto {
   @IsString()
+  @ApiProperty({ description: 'The title of the book', example: 'The Hobbit' })
   title: string;
 
   @IsString()
+  @ApiProperty({ description: 'The description of the book', example: 'The Hobbit is a fantasy book' })
   authorId: string;
 
   @IsDate()
+  @ApiProperty({ description: 'The date when the book was released. Formatted as ISO 8601', example: '1937-09-21T00:00:00.000Z' })
   @Type(() => Date)
   releasedAt: Date;
 
   @IsString({ each: true })
+  @ApiProperty({ description: 'The genres of the book', example: ['Fantasy', 'Adventure'] })
   genreNames: string[];
 }
