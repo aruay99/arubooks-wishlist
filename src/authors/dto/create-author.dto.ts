585731b735124290e46b52f22628ec106af23632
aruay99 <aruaiberdk@gmail.com>
2023-09-18 23:03:06 +0300

    docs(openapi): add CreateAuthorDto examples

--- src/authors/dto/create-author.dto.ts
+++ src/authors/dto/create-author.dto.ts
@@ -1,10 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsOptional } from 'class-validator';
 
 export class CreateAuthorDto {
   @IsString()
+  @ApiProperty({ description: 'The name of the author', example: 'John Doe' })
   name: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({
+    description: 'The description of the author',
+    example: 'John Doe is a very famous author'
+  })
   description?: string;
 }
